9c0c9cc9d0728b85e969a1a219c5b7421305b257
Ryan Highley <rhighley@redhat.com>
2022-11-04 11:17:40 -0500

    Updated for configurable stationId, Configured -in to run one message (https://camel.apache.org/blog/2020/07/command-line-utility-with-camel-quarkus/)

--- demo-weather-in/src/main/java/com/redhat/rhc/stp/ei/demo/weather/InboundRouteBuilder.java
+++ demo-weather-in/src/main/java/com/redhat/rhc/stp/ei/demo/weather/InboundRouteBuilder.java
@@ -1,25 +1,33 @@
 package com.redhat.rhc.stp.ei.demo.weather;
 
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.netty.http.NettyHttpOperationFailedException;
 
 public class InboundRouteBuilder extends RouteBuilder {
+
+    @PropertyInject(value = "weather.station", defaultValue = "kdfw")
+    String weatherStation;
     
     @Override
     public void configure() throws Exception {
 
-        from("timer:initiator?repeatCount=1")
-            .routeId("kdfw-latest-observation-route")
+        from("timer:initiator?repeatCount=1&delay=-1")
+            .routeId(String.format("%s-latest-observation-route", weatherStation))
             .onException(NettyHttpOperationFailedException.class)
                 .handled(true)
-                .to("log:kdfw-latest-observation-error?level=ERROR&showAll=true&multiline=true")
+                .toD("log:{{ weather.station }}-latest-observation-error?level=ERROR&showAll=true&multiline=true")
             .end()
-            .setHeader("stationId", constant("KDFW"))
+            .setHeader("weatherStation", constant(weatherStation))
+            .setHeader("stationId", constant(weatherStation.toUpperCase()))
+            .setHeader(KafkaConstants.KEY, header("stationId"))
+            .setHeader(KafkaConstants.HEADERS, constant("weatherStation=" + weatherStation))
             .setHeader("User-Agent", constant("(RHC-weather-demo, rhighley@redhat.com)"))
             .to(ExchangePattern.InOut, "rest-openapi:api.weather.gov/openapi.json#station_observation_latest")
             .to("log:demo-weather-in?showAll=true&multiline=true")
-            .to("kafka:weather-latest-observations-kdfw")
+            .toD("kafka:weather-latest-observations-${properties:weather.station:kdfw}")
             ;
     }
 }
--- demo-weather-transform/src/main/java/com/redhat/rhc/stp/ei/demo/weather/TransformRouteBuilder.java
+++ demo-weather-transform/src/main/java/com/redhat/rhc/stp/ei/demo/weather/TransformRouteBuilder.java
@@ -1,16 +1,22 @@
 package com.redhat.rhc.stp.ei.demo.weather;
 
+import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
+
 import org.apache.camel.builder.RouteBuilder;
 
 public class TransformRouteBuilder extends RouteBuilder {
 
+    @Inject
+    ConnectionFactory connectionFactory;
+
     @Override
     public void configure() throws Exception {
-        from("kafka:weather-latest-observations-kdfw")
-            .routeId("kdfw-transform-route")
+        from("kafka:weather-latest-observations-[a-z0-9]+?topicIsPattern=true")
+            .routeId("weather-latest-transform-route")
             .transform().jsonpath("$.properties.rawMessage")
-            .to("log:kdfw-transform?showAll=true&multiline=true")
-            .to("amqp:topic:weather.latest.kdfw")
+            .to("log:weather-latest-transform?showAll=true&multiline=true")
+            .toD("amqp:topic:weather.latest.kdfw")
             ;
     }
     
